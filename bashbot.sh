592336e401da5e03aa34026544f1e223876c1b6a
Musee U <icelandisbeingcolouredbywrens@gmail.com>
2012-05-27 20:38:40 -0500

    Basic response code checking

--- bashbot.sh
+++ bashbot.sh
@@ -36,18 +36,51 @@ startup() {
   f=/tmp/irc.$(date +%s)
   exec 3>$f
   echo -e "NICK $nick\nUSER $nick $nick $server :bashbot by musee" >&3
+  #set up capture from irc
+  log=/tmp/irc-reply.$(date +%s)
+  exec 4>$log
   #open connection to irc
-  tail -f $f | nc $server $port &
+  tail -f $f | nc $server $port >&4 &
   nc_pid=$!
+  parselog &
 }
 
 quit() {
   echo -e "\nQuitting..."
   kill -6 $nc_pid 2>&1>/dev/null
-  #remove file descriptor
+  #remove file descriptors
+  exec 4>&-
+  rm -f $log
   exec 3>&-
   rm -f $f
   exit $?
+}
+
+parselog() {
+  tail -f $log |
+  while read l
+  do
+    echo "$l" | grep ^: 2>&1>/dev/null
+    if [ $? -eq 0 ]; then
+      echo "$l" | grep -P "^:$server [0-9]{3} " 2>&1>/dev/null
+      if [ $? -eq 0 ]; then
+        r="${l##:$server }"
+        rc=${r%% $nick*}
+        r="`echo "$r" | sed "s/$nick //"`"
+        rp="${r#[0-9]* }"
+        case $rc in
+        *)
+          m="-?- $r"
+        ;;
+        esac
+      else
+        m="$l"
+      fi
+    else
+      m="$l"
+    fi
+    echo "$m"
+  done
 }
 
 # trap interrupt
